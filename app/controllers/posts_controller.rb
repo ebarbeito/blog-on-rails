62c762dd0439eb0207865df1ef620b78dccc9777
Enrique Barbeito García <ebarbeito@gmail.com>
2013-12-27 13:40:15 +0100

    from adding links till deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,44 @@
 class PostsController < ApplicationController
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def index
     @posts = Post.all
   end
 
   def new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+  	@post = Post.new
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+ 
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   private
